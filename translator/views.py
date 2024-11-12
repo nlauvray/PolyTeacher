769f31ac60d0322b7e0e4ddcb1c752cd45cca15e
Noa Lauvray <noalauvray@gmail.com>
2024-11-12 09:21:43 +0100

    clone_repo

---
+++ translator/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from translator.models import Translation
+from translator.serializers import TranslationSerializer
+
+# Create your views here.
+
+class FrenchSpanishTranslationViewSet(APIView):
+
+    def get(self, request):
+        return Response(data={}, status=None)
+    
+    def post(self, request):
+        return Response(data={}, status=None)
+    
+    def put(self, request, pk):
+        return Response(data={}, status=None)
+    
+    def delete(self, request, pk):
+        return Response(data={}, status=None)
+    
+class FrenchEnglishTranslationViewSet(APIView):
+
+    def get(self, request):
+        return Response(data={}, status=None)
+    
+    def post(self, request):
+        return Response(data={}, status=None)
+    
+    def put(self, request, pk):
+        return Response(data={}, status=None)
+    
+    def delete(self, request, pk):
+        return Response(data={}, status=None)
+
+def index(request):
+    return render(request, 'index.html', context={})
